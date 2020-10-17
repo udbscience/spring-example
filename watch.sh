caf2fcd974d213eca456533dd4710cccf5e11900
udbscience <udb.science@gmail.com>
2020-10-16 22:45:45 -0400

    Update watch.sh

--- watch.sh
+++ watch.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # watch the java files and continously deploy the service
-mvn clean install
-skaffold run -p dev
-reflex -r "\.java$" -- bash -c 'mvn install && skaffold run -p dev'
+# mvn clean install
+# skaffold run -p dev
+# reflex -r "\.java$" -- bash -c 'mvn install && skaffold run -p dev'
 
-
+echo "hello" >> /tmp/text.txt
 
